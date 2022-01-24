2bfbf76f7aaddcc6d830a9bb88be8a4952bf87cf
Tzuken <irriotzuken@gmail.com>
2022-01-24 22:17:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const Discord = require('discord.js')
 const Command = requiere('./command/command')
 const Help = requiere('./command/help')
 
-bot.on('message', message => {
-
-    Help.parse(message)
-
-})
-
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 client.once('ready', () =>{
