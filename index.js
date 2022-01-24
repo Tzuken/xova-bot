8b1e1e0cf629e0df8d5fb4de2141484de9a9a0d1
Tzuken <irriotzuken@gmail.com>
2022-01-24 22:39:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js')
 
-const Command = requiere('./command/command')
-
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 client.once('ready', () =>{
