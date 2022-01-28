4b2afe8fa240618323b50753c1a3a53ef5f856cd
Tzuken <irriotzuken@gmail.com>
2022-01-28 20:18:49 +0100

    modif

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const Discord = require('discord.js')
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
+const Help = require('./command/help')
+
 client.once('ready', () =>{
     console.log('Ready !')
 });
+
+
+bot.on('message', function (message) {
+ 
+    if (Help.match(message)) {
+        Help.action(message)
+    }
+
+})
 
 client.login(process.env.token);
