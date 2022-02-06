02b6cf48037e49281d1c773d5ff4e85b89d24f8c
Tzuken <irriotzuken@gmail.com>
2022-02-06 17:28:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 
 const Discord = require('discord.js');
 
-const config = require('./config.js');
-
 const intents = new Discord.Intents(32767);
 
 const client = new Discord.Client({ intents: intents });
