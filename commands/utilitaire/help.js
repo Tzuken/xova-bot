28a5b8806d89165763e225221ffe4ac101846f42
Tzuken <irriotzuken@gmail.com>
2022-02-06 17:22:07 +0100

    Update help.js

--- commands/utilitaire/help.js
+++ commands/utilitaire/help.js
@@ -1,1 +1,16 @@
 //commande help
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: 'Commande aide',
+    exemple: `**+help**`,
+    usage: '<aucun>',
+    guildOnly: true,
+    cooldown: 5,
+    async execute(client, message, args) {
+        message.delete();
+
+        message.channel.send(`${message.author.username}, voici toute les commandes disponible avec le bot !`)
+    }
+}
